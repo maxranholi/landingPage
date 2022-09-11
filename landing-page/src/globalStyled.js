f642596829bca9c9303ba2b5db202f2daba38f10
Max-Ranholi <ranholimax@gmail.com>
2022-09-11 17:08:29 -0300

    align done

--- landing-page/src/globalStyled.js
+++ landing-page/src/globalStyled.js
@@ -6,13 +6,13 @@ const GlobalStyle = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700;900&family=Splash&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;700;900&family=Splash&display=swap');
     font-family: 'Poppins', sans-serif;
-    font-size: 16px;
 }
 
 body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-size: 16px;
 }
 `;
 
